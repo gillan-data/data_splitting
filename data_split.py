58c2902e6f794def0f24836f3787af3df07ca023
Gillan Krishna <gillan.krishna@gmail.com>
2022-05-05 23:56:05 +0530

    Update data_split.py

--- data_split.py
+++ data_split.py
@@ -3,10 +3,13 @@ import random
 import os
 directory = os.getcwd()
 dirpath = os.path.join(directory, 'Sample_data')
-destDirectory = os.path.join(directory, 'split_2')
 
-filenames = random.sample(os.listdir(dirpath), 2)
-for fname in filenames:
-    srcpath = os.path.join(dirpath, fname)
-    destPath = os.path.join(destDirectory, fname)
-    shutil.copyfile(srcpath, destPath)
+j = [240, 960, 1920, 3840, 7680, 8640]
+
+for i in j:
+    destDirectory = os.path.join(directory, 'split_'+str(i))
+    filenames = random.sample(os.listdir(dirpath), i)
+    for fname in filenames:
+        srcpath = os.path.join(dirpath, fname)
+        destPath = os.path.join(destDirectory, fname)
+        shutil.copyfile(srcpath, destPath)
